1f798f9e070b58b6e68bd588008fed4b966fd946
IsraelAdeniran <B00157067@mytudublin.ie>
2025-04-15 20:16:03 +0100

    Registered Models in admin.py

--- Deliverable-3/retail_training/core/admin.py
+++ Deliverable-3/retail_training/core/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import *
 
-# Register your models here.
+# Custom UserAdmin
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('username', 'email', 'role', 'is_staff')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'role')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'role', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Admin)
+admin.site.register(Manager)
+admin.site.register(Trainer)
+admin.site.register(Employee)
+admin.site.register(Department)
+admin.site.register(TrainingModule)
+admin.site.register(Assignment)
+admin.site.register(Completion)
+admin.site.register(Feedback)
+admin.site.register(FeedbackResponse)
