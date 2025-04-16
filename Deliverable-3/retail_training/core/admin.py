5a60e316052bb3a890a3510df52455a0d51d0185
IsraelAdeniran <B00157067@mytudublin.ie>
2025-04-17 00:51:31 +0100

    Made a Custom User Creation Form in admin.py

--- Deliverable-3/retail_training/core/admin.py
+++ Deliverable-3/retail_training/core/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-from .models import *
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import User, Admin, Manager, Trainer, Employee, Department, TrainingModule, Assignment, Completion, Feedback, FeedbackResponse
+
+# Custom User Form
+from django import forms
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'role')
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if not first_name:
+            raise forms.ValidationError('First name is required.')
+        return first_name
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+        if not last_name:
+            raise forms.ValidationError('Last name is required.')
+        return last_name
+
 
 # Custom UserAdmin
 class UserAdmin(BaseUserAdmin):
@@ -17,11 +38,13 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'role', 'password1', 'password2'),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'role', 'password1', 'password2'),
         }),
     )
+
     search_fields = ('username', 'email')
     ordering = ('username',)
+    add_form = CustomUserCreationForm
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Admin)
